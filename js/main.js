071847bbad19fddff4dfdf10c179c9472c8e4284
Ian Veldhuizen <36740749+IanVeldhuizen@users.noreply.github.com>
2018-05-11 11:28:29 +0100

    Update Main JS

--- js/main.js
+++ js/main.js
@@ -12,44 +12,23 @@ lightbox.option({
 
 //======================== search
 
-$(document).ready(function () {
-
-const $search = $("#userSearch");
-const $img = $(this).find("a").attr("data-title");
-
-$($search).keyup(function() {
-
-    const $result = $($search).val().toLowerCase();
-    
-    for (let i = 0; i < $img.length; i++) {
-        const $title = $($img[i].getAttribute("data-title"));
-        if ($title.toLowerCase().includes($result)) {
-            $($img[i]).show();
-        }else {
-            $($img[i]).hide();
-        }
-    }
-})
-
-});
-
-/* ORIGINAL SUMBIT
-
-$(document).ready(function () {
-    $("#userSearch").keyup(function () {
-        const $result = $(this).val().toLowerCase();
-        $(".gallery_img").hide();
-        $(".gallery_img").each(function () {
-            const $img = $(this).text().toLowerCase();
-
-            if ($img.indexOf($result) != -1) {
-                $(this).show();
-            }
-        });
-    });
+const search = document.getElementById("userSearch");
+const img = document.querySelectorAll("a[data-title]");
 
 
-});
+ search.addEventListener("keyup", function(event) {
 
-*/
+     let result = search.value.toLowerCase();
+
+     for (let i = 0; i < img.length; i++){
+        let title = img[i].getAttribute("data-title");
+         if (title.toLowerCase().includes(result)) {
+             img[i].style.display = '';
+
+         } else {
+              img[i].style.display = 'none';
+            
+         }
+     }
+ });
 
